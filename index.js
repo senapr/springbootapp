b66b52a1e01d68e17c5584eb436727a174c0398a
Pradeep Sena <pradeepsena07@gmail.com>
2025-12-15 22:08:49 -0500

    Added fnctions

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const age = 24;
-const name = "Alice";
-console.log(`My name is ${name} and I am ${age} years old.`);
+const userName = "Alice";
+//console.log(`My name is ${name} and I am ${age} years old.`);
+
+function greet(userName) {
+    return `Hello, ${userName}! Welcome to our website.`;
+}
+
+function isAdult(userAge) {
+    return userAge >= 18;
+}
